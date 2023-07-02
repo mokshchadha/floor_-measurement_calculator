bef7e515e8684b2967a7df63c68e50019e3e1ec9
moksh chadha <mokshchadha@192.168.1.5>
2023-07-02 12:29:07 +0530

    pwa minimal setup

--- vite.config.js
+++ vite.config.js
@@ -3,56 +3,31 @@ import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
 
 const manifestforPlugin = {
-  registerType: "prompt",
-  includeAssets: ["calc.png", "192.png", "512.png"],
-  icons: [
-    {
-      src: "192.png",
-      size: "192x192",
-      type: "image/png",
-    },
-    {
-      src: "512.png",
-      size: "512x512",
-      type: "image/png",
-    },
-  ],
+  registerType: "autoUpdate",
+  devOptions: {
+    enabled: true,
+  },
+  includeAssets: ["192.png", "512.png"],
   manifest: {
-    name: "Floor Measurement Calculator",
-    short_name: "Floor Calculator",
-    description:
-      "An app that can calculate and print pdfs of your floor calculations",
-    type: "image/png",
-    purpose: "any maskable",
-    theme_color: "#171717",
-    background_color: "#eebf2",
-    display: "standalone",
-    scope: "/",
-    start_url: "/",
-    orientation: "portrait",
+    name: "My Awesome App",
+    short_name: "MyApp",
+    description: "My Awesome App description",
+    theme_color: "#ffffff",
+    icons: [
+      {
+        src: "192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        src: "512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+    ],
   },
 };
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), VitePWA(manifestforPlugin)],
-  // build: {
-  //   rollupOptions: {
-  //     input: {
-  //       // the default entry point
-  //       app: "./index.html",
-
-  //       // 1️⃣
-  //       serviceWorker: "./serviceWorker.js",
-  //     },
-  //     output: {
-  //       // 2️⃣
-  //       entryFileNames: (assetInfo) => {
-  //         return assetInfo.name === "serviceWorker"
-  //           ? "[name].js" // put service worker in root
-  //           : "assets/js/[name]-[hash].js"; // others in `assets/js/`
-  //       },
-  //     },
-  //   },
-  // },
 });
